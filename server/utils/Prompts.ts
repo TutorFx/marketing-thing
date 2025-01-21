f68f5f83c81a7e9f995e31de27236b9aa8d0c7f6
Gabriel Serejo <gabriel_serejo@live.com>
2025-01-20 23:01:30 -0300

    fix: costum prefix path on public importing

--- server/utils/Prompts.ts
+++ server/utils/Prompts.ts
@@ -1,10 +1,17 @@
 import { readFile } from 'node:fs/promises'
+import process from 'node:process'
 import { fileURLToPath } from 'node:url'
 
 export function getPromptFileData(fileName: string) {
+  let prefix = '.'
+
+  if (process.env.dev) {
+    prefix = '../..'
+  }
+
   return readFile(
     fileURLToPath(
-      new URL(`../../public/prompts/${fileName}.md`, import.meta.url),
+      new URL(`${prefix}/public/prompts/${fileName}.md`, import.meta.url),
     ),
     'utf-8',
   )
