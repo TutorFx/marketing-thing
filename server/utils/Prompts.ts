2cd374d964d91558fb079d96ce8633f52a15e3a6
Gabriel Serejo <gabriel_serejo@live.com>
2025-01-20 23:14:21 -0300

    feat: add vercel filesystem support

--- server/utils/Prompts.ts
+++ server/utils/Prompts.ts
@@ -3,15 +3,19 @@ import process from 'node:process'
 import { fileURLToPath } from 'node:url'
 
 export function getPromptFileData(fileName: string) {
-  let prefix = '.'
+  let prefix = './public/prompts/'
 
   if (process.env.dev) {
-    prefix = '../..'
+    prefix = '../../public/prompts/'
+  }
+
+  if (process.env.VERCEL_DEPLOYMENT_ID) {
+    prefix = './prompts/'
   }
 
   return readFile(
     fileURLToPath(
-      new URL(`${prefix}/public/prompts/${fileName}.md`, import.meta.url),
+      new URL(`${prefix}${fileName}.md`, import.meta.url),
     ),
     'utf-8',
   )
